7e5dfaabe85a156c2923c1a4588fd665309bc89f
penguinnnnn <hrz@pku.edu.cn>
2023-08-07 13:43:41 +0800

    Format

--- run_psychobench.py
+++ run_psychobench.py
@@ -7,8 +7,6 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--model', required=True, type=str, default='text-davinci-003',
                         help='The name of the model to test')
-    # parser.add_argument('--questionnaire', required=True, type=str,
-    #                     choices=['BFI', 'DTDD', 'EPQ-R', 'ECR-R', 'VIS', 'GSE', 'LMS', 'BSRI', 'ICB', 'LOT-R', 'Empathy', 'EIS', 'WLEIS'])
     parser.add_argument('--questionnaire', required=True, type=str,
                         help='Comma-separated list of questionnaires')
     parser.add_argument('--shuffle-count', required=True, type=int, default=0,
@@ -19,7 +17,7 @@ if __name__ == '__main__':
                         help='Name of this run. Is used to name the result files.')
     parser.add_argument('--significance-level', type=float, default=0.01,
                         help='The significance level for testing the difference of means between human and LLM. Defaults to 0.01.')
-    parser.add_argument('--mode', required=False, type=str, default='auto',
+    parser.add_argument('--mode', type=str, default='auto',
                         help='For debugging.')
 
     # Generator-specific parameters, can be discarded if users implement their own generators
